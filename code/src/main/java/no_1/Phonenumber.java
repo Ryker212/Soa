dcf1c9f05bb47238fcfd110a5bb1ece7d155d328
kurachatsarakarn <152004296+kurachatsarakarn@users.noreply.github.com>
2023-12-12 11:43:22 +0700

    Add files via upload

---
+++ code/src/main/java/no_1/Phonenumber.java
@@ -0,0 +1,25 @@
+package no_1;
+
+import java.util.ArrayList;
+
+public class Phonenumber  {
+	String number;
+	
+	
+	public Phonenumber(String num) {
+			this.number = num;
+		}
+	public Phonenumber() {
+
+	}
+
+
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+}
